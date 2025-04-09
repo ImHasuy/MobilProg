9e7eb54bfb34210f6fe57d133e7ee2cc40a3ea0c
Gáspár Csanád <RD7NAM@student.uni-pannon.hu>
2025-04-09 18:13:28 +0200

    First activity nearly functional, Act2 created

--- folder/app/src/main/java/com/example/mobilprog/MainActivity.java
+++ folder/app/src/main/java/com/example/mobilprog/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.mobilprog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    List<String> list;
+    ArrayAdapter<String> adapter;
+    EditText myedittext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,81 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String[] array = {"0.5 km", "1 km", "3 km", "5 km", "10 km"};
+        list = new ArrayList<>(Arrays.asList(array));
+        ListView listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        myedittext = findViewById(R.id.ListaEditTextje);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int
+                    position, long id) {
+                String selectedItem = list.get(position);
+                myedittext.setText(selectedItem);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View
+                    view, final int position, long id) {
+                String selectedItem = list.get(position);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Confirmation")
+                        .setMessage("Biztos ki akarod törölni az alábbi elemet: " + selectedItem)
+                        .setPositiveButton("Igen", new
+                                DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                list.remove(position);
+                                adapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("Nem", new
+                                DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int
+                                    id) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builder.create().show();
+                        return true;
+            }
+        });
+
+
     }
+
+    public void hozzaadas(View view) {
+        myedittext = findViewById(R.id.ListaEditTextje);
+        list.add(String.valueOf(myedittext.getText()));
+        adapter.notifyDataSetChanged();
+    }
+
+
+    public void torles(View view) {
+        myedittext = findViewById(R.id.ListaEditTextje);
+        String itemToRemove = String.valueOf(myedittext.getText());
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(itemToRemove)) {
+                list.remove(i);
+                adapter.notifyDataSetChanged();
+                break;
+            }
+        }
+    }
+
+    public void utolsotorles(View view) {
+        if (list.size() > 0) {
+            list.remove(list.size() - 1);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+
+
 }
